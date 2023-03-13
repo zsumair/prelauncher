a4f22f3e6545d3ccccf9fea55e7d76e6b207c8f4
Zoheb Sumair <syedzoheb1242@gmail.com>
2023-03-13 14:35:04 +0530

    fix: minor bug

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import "react-toastify/dist/ReactToastify.css";
 import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs";
 import { SessionContextProvider } from "@supabase/auth-helpers-react";
 import { useState } from "react";
+import Script from "next/script";
 import { ToastContainer } from "react-toastify";
 import NextNProgress from "nextjs-progressbar";
 import { AnimatePresence, motion } from "framer-motion";
@@ -12,34 +13,51 @@ export default function App({ Component, pageProps }) {
   const [supabaseClient] = useState(() => createBrowserSupabaseClient());
   const router = useRouter();
   return (
-    <SessionContextProvider
-      supabaseClient={supabaseClient}
-      initialSession={pageProps.initialSession}
-    >
-      <NextNProgress color="#EADDCD" startPosition={0.5} />
-      <AnimatePresence mode="wait">
-        <motion.div
-          key={router.route}
-          initial="initialState"
-          animate="animateState"
-          exit="exitState"
-          transition={{
-            duration: 0.5,
-          }}
-          variants={{
-            initialState: {
-              opacity: 0,
-            },
-            animateState: {
-              opacity: 1,
-            },
-            exitState: { opacity: 0 },
-          }}
-        >
-          <Component {...pageProps} />
-        </motion.div>
-      </AnimatePresence>
-      <ToastContainer />
-    </SessionContextProvider>
+    <>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+
+      <Script id="google-analytics" strategy="lazyOnload">
+        {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+                page_path: window.location.pathname,
+              });
+                  `}
+      </Script>
+      <SessionContextProvider
+        supabaseClient={supabaseClient}
+        initialSession={pageProps.initialSession}
+      >
+        <NextNProgress color="#EADDCD" startPosition={0.5} />
+        <AnimatePresence mode="wait">
+          <motion.div
+            key={router.route}
+            initial="initialState"
+            animate="animateState"
+            exit="exitState"
+            transition={{
+              duration: 0.5,
+            }}
+            variants={{
+              initialState: {
+                opacity: 0,
+              },
+              animateState: {
+                opacity: 1,
+              },
+              exitState: { opacity: 0 },
+            }}
+          >
+            <Component {...pageProps} />
+          </motion.div>
+        </AnimatePresence>
+        <ToastContainer />
+      </SessionContextProvider>
+    </>
   );
 }
